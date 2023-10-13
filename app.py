b8402c8757cca57558d938382a157d01cbd01ced
Anjan Biswas <anjanavb@amazon.com>
2023-10-12 20:59:35 -0700

    Initial commit

--- app.py
+++ app.py
@@ -4,8 +4,6 @@ import aws_cdk as cdk
 from stack.simple_async_workflow import SimpleAsyncWorkflow
 
 app = cdk.App()
-SimpleAsyncWorkflow(app, "SimpleAsyncWorkflow", env=cdk.Environment(
-    account=os.environ["CDK_DEFAULT_ACCOUNT"],
-    region=os.environ["CDK_DEFAULT_REGION"]))
+SimpleAsyncWorkflow(app, "SimpleAsyncWorkflow")
 
 app.synth()
